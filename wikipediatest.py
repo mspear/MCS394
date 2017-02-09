2124de37463d371f680eefadc1f632cd4540ef50
Michael Spear <mwspear@gmail.com>
2017-02-09 16:02:18 -0600

    Initial commit

---
+++ wikipediatest.py
@@ -0,0 +1,21 @@
+__author__ = 'mspear'
+from selenium import webdriver
+
+import time
+
+
+driver = webdriver.Chrome('/Users/mspear/Downloads/chromedriver')
+driver.get('http://www.wikipedia.org')
+time.sleep(5)
+
+link = driver.find_element_by_partial_link_text('English')
+link.click()
+
+time.sleep(5)
+
+search_box = driver.find_element_by_id('searchInput')
+search_box.send_keys('Hello Kitty')
+search_box.submit()
+time.sleep(5)
+print(driver.page_source)
+driver.quit()
