8af375d849430954e364ab25975162f4fdbf5b94
Michael Spear <mwspear@gmail.com>
2017-03-02 18:33:03 -0600

    rewrote cluster coefficient myself

--- facebook.py
+++ facebook.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import time
-import logging
 import networkx as nx
 
 
@@ -10,17 +9,12 @@ def find_friends(driver):
     time.sleep(5)
     try:
         driver.find_element('name', 'Mutual Friends').click()
-        print('made it')
     except:
         pass
     friends = driver.find_elements_by_class_name('_698')
     return friends
 
 
-# def process_friend(friend):
-#     friend_name = friend.find_element_by_tag_name('a')
-#     tmp = graph.Node(name=friend_name.text,link=friend_name.get_attribute('href'))
-#     return tmp
 def process_friend(friend):
     friend_name = friend.find_element_by_tag_name('a')
     return friend_name.get_attribute('href')
@@ -28,9 +22,6 @@ def process_friend(friend):
 
 def make_graph():
     driver = webdriver.Chrome('/Users/mspear/Downloads/chromedriver')
-    # driver.get('chrome://settings/advanced')
-    # driver.find_element_by_id('privacyContentSettingsButton').click()
-    # driver.find_element_by_name('popups').click()
     driver.get('http://www.facebook.com')
     time.sleep(5)
     email = driver.find_element_by_id('email')
@@ -46,43 +37,29 @@ def make_graph():
     time.sleep(10)
 
     friends = find_friends(driver)
-    # g = graph.Graph()
+
     g = nx.Graph()
 
-    # start = graph.Node('Michael Spear', 'https://www.facebook.com/michael.spear.94')
     g.add_node('https://www.facebook.com/michael.spear.94')
 
-
-    # g.add_node(1, link='https://www.facebook.com/michael.spear.94')
-    # g.add_node(start)
     queue = []
     for lv in friends:
-        # V = process_friend(lv)
-        # V.add_neighbor(start)
-        # start.add_neighbor(V)
-        # queue.append(V)
-        # logging.info(V.link)
-        # g.add_node(V)
         lnk = process_friend(lv)
-        g.add_node(lnk)
+        # g.add_node(lnk)
         g.add_edge('https://www.facebook.com/michael.spear.94', lnk)
         queue.append(lnk)
 
     while queue:
         time.sleep(10)
         current = queue.pop()
-        logging.info(f"Redirecting to {current}")
         driver.get(current)
         friends = find_friends(driver)
         for lv in friends:
             V = process_friend(lv)
             if V in g:
-                # tmp = g.get(V.link)
-                # tmp.add_neighbor(current)
-                # current.add_neighbor(tmp)
                 g.add_edge(current, V)
             else:
-                g.add_node(V)
+                # g.add_node(V)
                 g.add_edges_from([('https://www.facebook.com/michael.spear.94', V), (current, V)])
 
     driver.quit()
--- graph.py
+++ graph.py
@@ -1,56 +1,17 @@
-# class Graph:
-#     def __init__(self):
-#         self.g = {}
-#
-#     def add_node(self, node):
-#         self.g[node.link] = node
-#
-#     def get(self, element):
-#         return self.g[element]
-#
-#     def __contains__(self, item):
-#         return item.link in self.g
-#
-#     def __len__(self):
-#         return len(self.g)
-#
-#     def __iter__(self):
-#         for lv in self.g:
-#             yield lv
-#
-#
-# class Node:
-#     def __init__(self, name, link):
-#         self.name = name
-#         self.link = link
-#         self.neighbors = []
-#
-#     def add_neighbor(self, neighbor):
-#         if neighbor not in self.neighbors:
-#             self.neighbors.append(neighbor)
-#
-#     def __hash__(self):
-#         return hash(self.link)
-#
-#     def __eq__(self, other):
-#         if isinstance(other, self.__class__):
-#             return self.link == other.link
-#         return False
-#
-#     def __ne__(self, other):
-#         return not self.__eq__(other)
-#
-#
-# def cluster_coefficient(base):
-#     f = base.neighbors
-#     numerator = 0
-#     for friend in f:
-#         numerator += (len(set(friend.neighbors).intersection(set(f))) - 1)
-#     return numerator/(len(f) * (len(f)+1)/2)
 import facebook
 import pickle
 import os
 import networkx as nx
+
+
+def cluster_coefficient(graph):
+    cc_dict = {}
+    for everynode in graph.nodes():
+        g_prime = graph.subgraph([everynode]+ list(graph[everynode].keys()))
+        k = nx.complete_graph(len(g_prime))
+        cc_dict[everynode] = len(g_prime.edges())/len(k.edges())
+
+    return cc_dict
 
 
 def show_graph(g):
@@ -58,8 +19,8 @@ def show_graph(g):
     nx.draw_networkx(g, with_labels=False)
     plt.show()
 
+
 def main():
-    import operator
     if os.path.isfile('/Users/mspear/graph'):
         with open('/Users/mspear/graph', 'rb') as f:
             g = pickle.load(f)
@@ -68,9 +29,7 @@ def main():
         with open('/Users/mspear/graph', 'wb') as f:
             pickle.dump(g, f)
 
-
-    cc = nx.clustering(g)
-    print(sorted(cc.items(), key=operator.itemgetter(1))[-5:])
-    print(sorted(nx.betweenness_centrality(g).items(), key=operator.itemgetter(1))[-5:])
+    cc = cluster_coefficient(g)
+    print(cc['https://www.facebook.com/cooper.nelson.904?fref=pb&hc_location=friends_tab'])
 if __name__ == '__main__':
     main()
