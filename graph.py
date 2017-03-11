8bd624b41036412083d0272a3f5e354269f8e4fa
Michael Spear <mwspear@gmail.com>
2017-03-11 10:08:43 -0600

    Did more things

--- graph.py
+++ graph.py
@@ -2,16 +2,7 @@ import facebook
 import pickle
 import os
 import networkx as nx
-
-
-def cluster_coefficient(graph):
-    cc_dict = {}
-    for everynode in graph.nodes():
-        g_prime = graph.subgraph([everynode]+ list(graph[everynode].keys()))
-        k = nx.complete_graph(len(g_prime))
-        cc_dict[everynode] = len(g_prime.edges())/len(k.edges())
-
-    return cc_dict
+import bin.algorithms as alg
 
 
 def show_graph(g):
@@ -29,7 +20,9 @@ def main():
         with open('/Users/mspear/graph', 'wb') as f:
             pickle.dump(g, f)
 
-    cc = cluster_coefficient(g)
-    print(cc['https://www.facebook.com/cooper.nelson.904?fref=pb&hc_location=friends_tab'])
+    cc = alg.cluster_coefficient(g)
+    bc = nx.betweenness_centrality(g)
+    print(bc)
+    print(cc)
 if __name__ == '__main__':
     main()
